dc1768c3fc28133f3653f18909ad79018ff21ed8
Paurush Rai <paurushrai99@gmail.com>
2026-04-23 03:59:55 +0530

    feat: add Microsoft Clarity analytics script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -111,6 +111,13 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           strategy="lazyOnload"
         />
       )}
+      <Script id="clarity-analytics" strategy="afterInteractive">
+        {`(function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "wk5kqembjf");`}
+      </Script>
       <body
         className={`bg-black ${
           process.env.NODE_ENV === "development" ? "debug-screens" : undefined
