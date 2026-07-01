e122db3ba3e13be83c2b55bec7463dfd3ed992e9
Paurush Rai <paurushrai99@gmail.com>
2026-07-01 23:40:34 +0530

    chore(tailwind): remove dev-only debug-screens plugin (v4-incompatible)

--- app/layout.tsx
+++ app/layout.tsx
@@ -133,11 +133,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
         })(window, document, "clarity", "script", "wk5kqembjf");`}
       </Script>
-      <body
-        className={`bg-black ${
-          process.env.NODE_ENV === "development" ? "debug-screens" : undefined
-        }`}
-      >
+      <body className="bg-black">
         {process.env.GTM_ID && (
           <noscript>
             <iframe
